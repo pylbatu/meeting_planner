cf87831c963ff611629a3e3407823cb647f7d4de
Pyl <pyl.jan.batu@gmail.com>
2020-04-29 12:39:12 +0800

    new fields

--- meetings/models.py
+++ meetings/models.py
@@ -1,3 +1,4 @@
+from datetime import time
 from django.db import models
 
 # Create your models here.
@@ -6,3 +7,8 @@ from django.db import models
 class Meeting(models.Model):
     title = models.CharField(max_length=40)
     date = models.DateField()
+    time = models.TimeField(default=time(9))
+    durationInHours = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f'Meeting {self.title} at {self.date} {self.time}'
