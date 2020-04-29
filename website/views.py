07aa3b5767fe383c4667b53aeead78dbcec49024
Pyl <pyl.jan.batu@gmail.com>
2020-04-29 11:42:20 +0800

    index view and date view

--- website/views.py
+++ website/views.py
@@ -1,8 +1,13 @@
+
+from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
-
 # Create your views here.
 
 
 def welcome(request):
-    return HttpResponse("Welcome to your website Pyl!")
+    return HttpResponse('Welcome to your website Pyl!')
+
+
+def date(request):
+    return HttpResponse('This request is made at ' + str(datetime.now()))
