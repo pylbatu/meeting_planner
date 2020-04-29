7177b22e1495308a27e7a03fc2fc6378ea515942
Pyl <pyl.jan.batu@gmail.com>
2020-04-29 14:17:11 +0800

    Second model class with relation

--- meetings/models.py
+++ meetings/models.py
@@ -4,11 +4,21 @@ from django.db import models
 # Create your models here.
 
 
+class Room(models.Model):
+    name = models.CharField(max_length=40)
+    floor_number = models.IntegerField()
+    room_number = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.name}'
+
+
 class Meeting(models.Model):
     title = models.CharField(max_length=40)
     date = models.DateField()
     time = models.TimeField(default=time(9))
-    durationInHours = models.IntegerField(default=1)
+    duration_in_hours = models.IntegerField(default=1)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'Meeting {self.title} at {self.date} {self.time}'
