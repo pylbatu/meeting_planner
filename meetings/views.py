99bfc36a1b0c93cbc02de0edf71066b497cc5cb7
Pyl <pyl.jan.batu@gmail.com>
2020-04-29 14:51:47 +0800

    url params: meeting detail

--- meetings/views.py
+++ meetings/views.py
@@ -1,3 +1,12 @@
+from .models import Meeting
+
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def detail(request, id):
+    meeting = Meeting.objects.get
+    return render(request, 'meetings/detail.html', {
+        'meeting': meeting
+    })
